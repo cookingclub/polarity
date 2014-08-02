e23f604f324f836079ef895923991bdd1b27f9f7
danielrh <danielrh@users.sourceforge.net>
2014-08-02 14:49:16 -0700

    added more of a class hierarchy to physics.hpp and windows linking commands

--- physics/physics.hpp
+++ physics/physics.hpp
@@ -1,9 +1,28 @@
 namespace Polarity {
+namespace Polarity {
   class GameObject {
-    Vector location;
+     Vector location;
+     Vector min; // relative to location
+     Vector max;
+     static bool collide(const GameObject &obj0, const GameObject &obj1, Vector2f& normal){
+         Vector bb0min = obj0.location - obj0.min;
+         Vector bb0max = obj0.location + obj0.max;
     
+      } 
   };
-  class Player: public GameObject {
-    Vector location;
+  class Movable :public GameObject {
+      Vector velocity;
   };
+  class Player: public Movable {
+      Vector location;
+  };
+  class PolygonalObject : public BoundedObject {
+     std::vector<Vector> vertices;
+  };
+
+
+ 
+ 
+ 
+  
 }
--- winbuild.cmd
+++ winbuild.cmd
@@ -1,5 +1,5 @@
 set EXE=polarity.html
-set OBJS=graphics/*.o main/*.o
+set SRCS=graphics\*.cpp main\*.cpp
 
 set CURDIR=%CD%
 
@@ -10,12 +10,12 @@ set CXX=em++
 
 set CFLAGS=-pthread -g -O2 -I %CURDIR%
 rem -Ilibtmx-parser/src -Itinyxml2
-set CXXFLAGS=-std=gnu++11 $(CFLAGS)
+set CXXFLAGS=-std=gnu++11 %CFLAGS%
 set LDFLAGS=-s STB_IMAGE=1 --preload-file assets
-
-for %%i in %SRCS% (
-    %CXX% %CXXFLAGS% %%i -c -o %%~dpni.o
-)
-
+echo %SRCS%
+set OBJS=
+for %%i in (%SRCS%) DO %CXX% %CXXFLAGS% %%~dpni.cpp -c -o %%~dpni.o
+   set OBJS= %OBJS% %%~dpni.o
+echo hi
 %CXX% %OBJS% %LDFLAGS% -o %EXE%
 
